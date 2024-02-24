4abfd52a7ea44a9aa2485ea6ebd72bc85789c5af
Kondwani Nyirenda <101860589+kondwani0099@users.noreply.github.com>
2024-02-24 21:15:20 +0200

    Add files via upload

---
+++ src/Components/Education.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Education = () => {
+    return (
+        <section className="section" id="education" style={{ backgroundColor: "#f0f1f5" }}>
+            <div className="container">
+                <h2 className="title has-text-centered">Education</h2>
+                <div className="content">
+                    <p> Bachelor's Degree in Mechatronics Engineering</p>
+                    <p> Secondary School: Chililabombwe Secondary</p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Education;
