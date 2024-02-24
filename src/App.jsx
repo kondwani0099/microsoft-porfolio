c022f2eb8da2db0aa869b90dcf8d85bc85c24407
Kondwani Nyirenda <101860589+kondwani0099@users.noreply.github.com>
2024-02-24 21:12:29 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Kondwani Nyirenda",
+  title: "Mechatronics & Software Developer",
+  email: "kondwaninyirenda99@gmail.com",
+  gitHub: "kondwani0099",
+  instagram: "kondwani0099",
+  linkedIn: "kondwani-nyirenda-907014246",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "lapansiindustries",
 };
 
 const primaryColor = "#4E567E";
